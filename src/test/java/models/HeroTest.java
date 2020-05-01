c480dcb499432eba5849f28a3fe0cfe0743fc6af
Blackscure <Wekesabuyahi@gmail.com>
2020-05-01 22:50:19 +0300

    Test for heroes

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -5,5 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class HeroTest {
+    @Test
+    public void testForHeroes() throws Exception{
+        Hero myHeroes = new Hero("Hulk",47, "Acuracy", "Love");
+        assertEquals(true, myHeroes instanceof Hero);
+    }
 
 }
