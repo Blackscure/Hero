c0e2f373a12840451096853e105774b3c96078e0
Blackscure <Wekesabuyahi@gmail.com>
2020-05-01 22:47:42 +0300

    test methods deleted

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -5,21 +5,5 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class HeroTest {
-    @Test
-    public void newHeroInstantiatesCorrectly_true(){
-        Hero hero= setUpNewHero();
-        assertEquals(true,hero instanceof Hero);
-    }
 
-    @Test
-    public void heroMustHaveAName(){
-        Hero hero =setUpNewHero();
-        assertEquals("hulk",hero.getName());
-    }
-
-    @Test
-    public void  getPowerChecksTheHerosPower(){
-        Hero hero = setUpNewHero();
-        assertEquals("height",hero.getPower());
-    }
 }
