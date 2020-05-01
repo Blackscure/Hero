a6b1dd6da577efb79f099a49929331273832516e
Blackscure <Wekesabuyahi@gmail.com>
2020-05-01 23:07:25 +0300

    Test Squad name

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,5 +1,14 @@
 package models;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SquadTest {
 
+    @Test
+    public void testForSquadName() throws Exception{
+        Squad mySquad = new Squad("Avengers",5,"protect Earth");
+        assertEquals("Avengers",mySquad.getName());
+    }
 }
